708f9c51df51979a5b0d95b7d51c998d9600d634
anis-meliti <ansmeliti@gmail.com>
2021-12-13 20:57:56 +0100

    first commit

---
+++ node_script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "Enable SSH password authentication:"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "Set root password:"
+echo -e "iamadmin\niamadmin" | passwd root >/dev/null 2>&1
+
+# Commands for all K8s nodes
+# Add Docker GPG key, Docker Repo, install Docker and enable services
+# Add repo and Install packages
+sudo apt-get update
+sudo  apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo  apt update && apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
+
+
+# Start and enable Services
+sudo systemctl daemon-reload 
+sudo systemctl enable docker
+sudo systemctl start docker
+
+#Confirm that docker group has been created on system
+sudo groupadd docker
+
+# Add your current system user to the Docker group
+sudo gpasswd -a $USER docker
+docker --version
+
+# Turn off swap
+# The Kubernetes scheduler determines the best available node on 
+# which to deploy newly created pods. If memory swapping is allowed 
+# to occur on a host system, this can lead to performance and stability 
+# issues within Kubernetes. 
+# For this reason, Kubernetes requires that you disable swap in the host system.
+# If swap is not disabled, kubelet service will not start on the masters and nodes
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+
+# Turn off firewall (Optional)
+ufw disable
+
+# Modify bridge adapter setting
+# Configure sysctl.
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+# Ensure that the br_netfilter module is loaded
+lsmod | grep br_netfilter
